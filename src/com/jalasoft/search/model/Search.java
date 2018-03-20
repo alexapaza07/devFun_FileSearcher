5d3bde18f553dc8ce497bc490f4b1ca5f0232959
alexapaza07 <37457705+alexapaza07@users.noreply.github.com>
2018-03-20 16:53:53 -0400

    Test File

---
+++ src/com/jalasoft/search/model/Search.java
@@ -0,0 +1,14 @@
+package com.jalasoft.search.model;
+
+public class Search {
+    public Search(){
+
+    }
+    public void listFileByPath()
+    {
+        String path= "c:\\test";
+        File files = new File(path);
+        
+    }
+
+}
